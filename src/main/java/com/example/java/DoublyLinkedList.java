5bebd2978e626d1a0ba61cf3ecb570151d34c5d6
DmytroHalai <dimagalai04@gmail.com>
2025-03-19 13:56:44 +0200

    Add insert method for list

--- src/main/java/com/example/java/DoublyLinkedList.java
+++ src/main/java/com/example/java/DoublyLinkedList.java
@@ -30,4 +30,48 @@ public class DoublyLinkedList {
             tail = newNode;
         }
     }
+
+    public void insert(char element, int index) throws Exception {
+        checkIndex(index);
+        Node newNode = new Node(element);
+
+        if (index == 0) {
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            }
+        } else if (index == length()) {
+            if (tail == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                newNode.prev = tail;
+                tail = newNode;
+            }
+        } else {
+            Node current = head;
+            int curentIndex = 0;
+            while (curentIndex < index) {
+                current = current.next;
+                curentIndex++;
+            }
+            newNode.next = current;
+            newNode.prev = current.prev;
+            if (current.prev != null) {
+                current.prev.next = newNode;
+            }
+            current.prev = newNode;
+        }
+    }
+
+    public void checkIndex(int index) throws Exception {
+        if (index < 0 || index > length()) {
+            throw new Exception("Index is not valid");
+        }
+    }
 }
