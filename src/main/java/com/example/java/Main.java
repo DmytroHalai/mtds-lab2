9606b6e35e9a770ecd18869a5c7f215b6564d135
DmytroHalai <dimagalai04@gmail.com>
2025-03-19 13:58:08 +0200

    Add example of using list

--- src/main/java/com/example/java/Main.java
+++ src/main/java/com/example/java/Main.java
@@ -1,5 +1,30 @@
 package com.example.java;
 
-
 public class Main {
+    public static void main(String[] args) throws Exception {
+        DoublyLinkedList list = new DoublyLinkedList();
+
+        list.append('1');
+        list.append('2');
+        list.append('3');
+        list.append('4');
+        list.printList();
+
+        System.out.println("Довжина списку: " + list.length());
+        list.insert('a', 2);
+        list.printList();
+
+        char res = list.delete(2);
+        System.out.println(res);
+        list.printList();
+
+        list.append('e');
+        list.append('e');
+        list.append('e');
+        list.append('e');
+        list.printList();
+        list.deleteAll('e');
+        list.printList();
+
+    }
 }
