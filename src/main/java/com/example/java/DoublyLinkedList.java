bb93f896e910031a125b30a781d43d4669883169
DmytroHalai <dimagalai04@gmail.com>
2025-03-19 13:55:45 +0200

    Add length() method for list

--- src/main/java/com/example/java/DoublyLinkedList.java
+++ src/main/java/com/example/java/DoublyLinkedList.java
@@ -1,4 +1,21 @@
 package com.example.java;
 
 public class DoublyLinkedList {
+    public Node head;
+    public Node tail;
+
+    public DoublyLinkedList() {
+        head = null;
+        tail = null;
+    }
+
+    public int length() {
+        int length = 0;
+        Node current = head;
+        while (current != null) {
+            length++;
+            current = current.next;
+        }
+        return length;
+    }
 }
