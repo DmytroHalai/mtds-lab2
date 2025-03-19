ff540553f9551aeb9ae6db8d1785b13708f9a1bb
DmytroHalai <dimagalai04@gmail.com>
2025-03-19 16:00:51 +0200

    Add unit test for method insert

--- src/main/java/com/example/java/Main.java
+++ src/main/java/com/example/java/Main.java
@@ -1,7 +1,7 @@
 package com.example.java;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         DoublyLinkedList list = new DoublyLinkedList();
 
         list.append('1');
--- src/test/java/DoublyLinkedListTest.java
+++ src/test/java/DoublyLinkedListTest.java
@@ -1,9 +1,16 @@
 import com.example.java.DoublyLinkedList;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class DoublyLinkedListTest {
+
+    @Test
+    void testDoublyLinkedList(){
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+        assertNull(doublyLinkedList.head, "Head must be null");
+        assertNull(doublyLinkedList.tail, "Tail must be null");
+    }
 
     @Test
     void testLength(){
@@ -18,5 +25,29 @@ class DoublyLinkedListTest {
         doublyLinkedList.append('1');
         result = doublyLinkedList.length();
         assertEquals(4, result, "Length of list of 4 elements must be 4");
+    }
+
+    @Test
+    void testAppend(){
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+
+        doublyLinkedList.append('1');
+        assertEquals(1, doublyLinkedList.length(), "Length must be 1");
+
+        doublyLinkedList.append('2');
+        assertEquals(2, doublyLinkedList.length(), "Length must be 2");
+        assertEquals('1', doublyLinkedList.head.data, "Head data must be '1'");
+        assertEquals('2', doublyLinkedList.tail.data, "Tail data must be '2'");
+    }
+
+    @Test
+    void testInsert() throws Exception {
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+
+        doublyLinkedList.insert('1', 0);
+        assertEquals(1, doublyLinkedList.length(), "Length must be 1");
+        assertEquals('1', doublyLinkedList.head.data, "Head data must be 1");
+
+        assertThrows(IndexOutOfBoundsException.class, () -> doublyLinkedList.insert('2', 3));
     }
 }
